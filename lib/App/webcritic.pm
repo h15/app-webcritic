c544abf1ef09de418e5922514073dca6a9ee2e7f
Georgy Bazhukov <georgy.bazhukov@gmail.com>
2013-06-11 10:52:31 +0600

    a few docs, readme

--- lib/App/webcritic.pm
+++ lib/App/webcritic.pm
@@ -12,5 +12,35 @@ App::webcritic - A critic for web-sites.
 
 =head1 OVERVIEW
 
+webcritic provides script for site analitic. It runs for you site and check site
+for some rules (planned).
+
+=head1 USAGE
+
+You can run webcritic from console line by this command:
+
+  ./script/webcritic -c /path/to/config.json
+
+The simple config describes site list, their urls (url for first page) and
+local site name.
+
+  {
+    "test": {
+      "url": "http://127.0.0.1:3000",
+      "name": "Test site"
+    },
+    "my first site": {
+      "url": "http://example.net",
+      "name": "My Site"
+    }
+  }
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013, Georgy Bazhukov.
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
 
 =cut
